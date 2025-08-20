de7511518b4cf910f4a5a5d3e6eacb7701981d78
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-08-20 17:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	 const fname = document.forms["nameForm"]["fname"].value.trim();
+      const lname = document.forms["nameForm"]["lname"].value.trim();
 
+      // Combine names
+      const fullName = fname + " " + lname;
+
+      // Show alert
+      alert(fullName);
+
+      // Prevent form from refreshing page
+      return false;
 }
